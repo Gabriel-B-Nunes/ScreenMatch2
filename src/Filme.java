45de5022f84ba9f74553bc947a56a950b0a8c168
GabrielBN <gabrielbeckernunes@gmail.com>
2024-03-08 13:20:07 -0400

    aula1

---
+++ src/Filme.java
@@ -0,0 +1,57 @@
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    void exibeFichaTécnica() {
+        System.out.println(String.format("""
+                    Nome do filme: %s
+                    Ano de lançamento: %d
+                    """, nome, anoDeLancamento));
+    }
+
+    void avalia(double nota) {
+        somaAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double getMedia() {
+        return somaAvaliacoes / totalDeAvaliacoes;
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+    public static void main(String[] args){
+        Filme filme1 = new Filme();
+
+        filme1.setNome("O Poderoso Chefão");
+        filme1.setAnoDeLancamento(1970);
+        filme1.setDuracaoEmMinutos(180);
+
+        filme1.exibeFichaTécnica();
+        filme1.avalia(8);
+        filme1.avalia(5);
+        filme1.avalia(10);
+
+        System.out.println(filme1.getMedia());
+    }
+}
